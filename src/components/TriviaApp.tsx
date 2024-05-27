c814e53c1152116671b6e8aef9920fbbc9bc9f0c
BorisTheBear <boris.j911@gmail.com>
2024-05-27 18:38:21 +0200

    zadatak 1

---
+++ src/components/TriviaApp.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TriviaApp:React.FC = () =>{
+    return(
+        <div>
+            <h1>Trivia App</h1>
+
+        </div>
+    )
+};
+export default TriviaApp; 
